a4e6fb299fa605308b2952720ee898b5d2eb6836
Diogo Silva <diogo@diogok.net>
2016-02-24 17:38:44 -0300

    git fix run script to not include tests

--- run.sh
+++ run.sh
@@ -4,17 +4,17 @@ if [ "$1" = "dev" ];
 then
   while :
   do
-    if [ -e ".token"]; 
+    if [ -e ".token" ]; 
     then
       export GITHUB_TOKEN=$(cat .token)
     fi
-    go run $(ls src/*.go) &
+    go run $(ls src/*.go | grep -v test ) &
     pid=$!
     sleep 10
     kill $pid
     kill $(ps | grep go-build | awk '{ print $1}' | head -n 1)
   done
 else
-  go run $(ls src/*.go)
+  go run $(ls src/*.go | grep -v test)
 fi
 
