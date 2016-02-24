52f17f5df2998b19c2496ff002833a519ed2302a
Diogo Silva <diogo@diogok.net>
2016-02-24 16:52:15 -0300

    most github functions done

--- src/github.go
+++ src/github.go
@@ -2,12 +2,14 @@ package main
 
 import (
   "github.com/google/go-github/github"
+	"math"
 )
 
 type Developer struct {
 	Name *string `json:"name,omitempty"`
 	ID *int `json:"id"`
 	Login *string `json:"login"`
+	Cost int `json:"cost"`
 }
 
 type Developers []Developer
@@ -47,6 +49,14 @@ func repositories(user string)  (Repositories,error) {
 	}
 }
 
+func calcCost(repos Repositories) int {
+	cost := 0
+	for _, repo := range repos {
+		cost = cost + (1 + ( *repo.Stars * 2) + ( *repo.Watchers * 3 ) + ( *repo.Forks * 4 ))
+	}
+	return int( math.Floor( float64( cost ) * 0.1 ) )
+}
+
 func developers(query string, page int) (Developers, error) {
 	client := github.NewClient(nil)
 
@@ -61,10 +71,13 @@ func developers(query string, page int) (Developers, error) {
 	} else {
 		devs := Developers{}
 		for _, user := range rdevs.Users {
+			repos,_ := repositories(*user.Login)
+			cost := calcCost(repos)
 			dev := Developer{
 				Login: user.Login,
 				Name: user.Name,
 				ID: user.ID,
+				Cost: cost,
 			}
 			devs = append(devs,dev)
 		}
--- src/github_test.go
+++ src/github_test.go
@@ -2,34 +2,44 @@ package main
 
 import (
 	"testing"
+	"fmt"
 )
 
 func TestRepositories(t *testing.T) {
 	repos, err := repositories("diogok")
 	if err != nil{
 		t.Error("Listing repos error")
-	}
-	if repos == nil {
+		fmt.Printf("%+v\n", err)
+	} else if repos == nil {
 		t.Error("Repos is nil")
-	}
-	if len(repos) != 50 {
+	} else if len(repos) != 50 {
 		t.Error("Wrong number of repos")
-	}
-	found := false
-	for _, repo := range repos {
-		if *repo.Name == "restserver" {
-			found = true
-			if *repo.Stars != 31 {
-				t.Error("Wrong star count")
-			}
-			if *repo.Forks != 9 {
-				t.Error("Wrong fork count")
+	} else {
+		found := false
+		for _, repo := range repos {
+			if *repo.Name == "restserver" {
+				found = true
+				if *repo.Stars != 31 {
+					t.Error("Wrong star count")
+				}
+				if *repo.Forks != 9 {
+					t.Error("Wrong fork count")
+				}
 			}
 		}
-	}
 
-	if found == false {
-		t.Error("Repo not found")
+		if found == false {
+			t.Error("Repo not found")
+		}
+	}
+}
+
+func TestCost(t *testing.T) {
+	repos, _ := repositories("diogok")
+	cost := calcCost(repos)
+	if cost != 55 {
+		t.Error("Wrong cost")
+		fmt.Printf("%d",cost)
 	}
 }
 
@@ -37,12 +47,16 @@ func TestDevelopers(t *testing.T) {
 	devs, err := developers("diogo",0)
 	if err != nil{
 		t.Error("Listing developers error")
-	}
-	if devs == nil {
+	} else if devs == nil {
 		t.Error("Devs is nil")
-	}
-	if len(devs) != 10 {
+	} else if len(devs) != 10 {
 		t.Error("Wrong number of devs")
+	} else {
+		devs, err = developers("diogok",0)
+		if devs[0].Cost != 55 {
+			t.Error("Problem calc cost in developers")
+			fmt.Printf("%+v\n", devs[0])
+		}
 	}
 }
 
