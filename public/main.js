0c1124456747b6d9a7517be3547754bdd55e4eb0
Diogo Silva <diogo@diogok.net>
2016-02-24 21:14:38 -0300

    develoepr listing ui

--- public/main.js
+++ public/main.js
@@ -1,8 +1,56 @@
 
 function main() {
-
   var index = Vue.extend({template:"#index"});
+
   var cart = Vue.extend({template:"#cart"});
+
+  var developers = Vue.extend({
+      template:"#developers"
+      ,methods: {
+        search: function() {
+          loading();
+          event.preventDefault();
+          var that=this;
+          superagent
+            .get('/developers?page='+this.page+'&query='+encodeURIComponent(this.query))
+            .end(function(err,res){
+              that.developers = JSON.parse(res.text);
+              if(that.developers.length >= 5) {
+                that.hasNext=true;
+              }
+              if(that.developers.length != 0) {
+                that.hasDevs=true;
+              } else{
+                that.hasDevs=false;
+              }
+              unloading();
+            });
+        }
+        ,next: function(){
+          event.preventDefault();
+          this.hasPrev=true;
+          this.page++;
+          this.search();
+        }
+        ,prev: function() {
+          event.preventDefault();
+          if(this.page > 1) {
+            this.hasNext=true;
+            this.page--;
+            this.search();
+          }
+        }
+        ,add: function() {
+          event.preventDefault();
+          loading();
+        }
+      }
+      ,data: function() {
+        return {
+          page:1,hasNext:false,hasPrev:false,hasDevs:false,query:"",developers: [ ]
+        }
+      }
+    });
 
   var app = Vue.extend({});
   var router = new VueRouter();
@@ -10,6 +58,7 @@ function main() {
   router.map({
      '/':{component: index}
     ,'/cart':{component: cart}
+    ,'/developers':{component: developers}
   });
 
   router.afterEach(function(){
@@ -18,6 +67,14 @@ function main() {
 
   router.start(app,".inner-content");
 
+}
+
+function loading() {
+  document.querySelector(".loading").classList.add('is-active');
+}
+
+function unloading() {
+  document.querySelector(".loading").classList.remove('is-active');
 }
 
 window.onload = main;
